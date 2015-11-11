9e6e045bad0ccd04165e2b7c2c31ac60baf48cb1
peter BigPc ubuntu <peter_BigPc_ubuntu@itu.edu>
2015-11-10 17:51:48 -0800

    aaa

--- main.py
+++ main.py
@@ -13,14 +13,22 @@
 # 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
 # 2. Run "python main.py".
 # 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, send_from_directory
 from camera import VideoCamera
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/css/<path:path>')
+def send_js(path):
+    return send_from_directory('css', path)
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
 
 def gen(camera):
     while True:
@@ -28,10 +36,15 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+@app.route('/forward')
+def forward_world():
+    # do some operation for the cart
+    return 'forward World!'
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='10.1.15.97', debug=True)
