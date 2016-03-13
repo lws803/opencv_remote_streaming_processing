8c8964b58a83ea9debaaf5c1270bf254701cb7bf
Hector <hector.cadavid@gmail.com>
2016-03-13 00:16:07 -0500

    client/server project structure

---
+++ client/streamreceiver2.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import urllib
+
+stream=urllib.urlopen('http://@192.168.1.8:5000/video_feed.mjpg')
+bytes=''
+while True:
+    bytes+=stream.read(1024)
+    a = bytes.find('\xff\xd8')
+    b = bytes.find('\xff\xd9')
+    if a!=-1 and b!=-1:
+        jpg = bytes[a:b+2]
+        bytes= bytes[b+2:]
+        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
+        cv2.imshow('i',i)
+        if cv2.waitKey(1) ==27:
+            exit(0) 
